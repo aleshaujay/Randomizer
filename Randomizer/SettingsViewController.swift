80e1ae741430ff55343624c7c852096f01704b6c
aleshaujay <aleshaujay@icloud.com>
2024-08-10 16:32:21 +0700

    I repeated the Randomizer application that was disassembled on Lesson 2.06

--- Randomizer/SettingsViewController.swift
+++ Randomizer/SettingsViewController.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
+    var minimumValue: String!
+    var maximumValue: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        minimumValueTF.text = minimumValue
+        maximumValueTF.text = minimumValue
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonAction() {
+        dismiss(animated: true)
     }
-    */
-
 }
--- Randomizer/ViewController.swift
+++ Randomizer/ViewController.swift
@@ -18,12 +18,25 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         getRandomNumberButton.layer.cornerRadius = 15
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("Go 2 Settings VC")
+        let settingsVC = segue.destination as? SettingsViewController
+        settingsVC?.minimumValue = minimumValueLabel.text
+        settingsVC?.maximumValue = maximumValueLabel.text
+    }
+    
     @IBAction func getRandomNumberButtonTapped() {
         let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
         let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
         
         randomValueLabel.text = Int.random(in: minimumNumber...maximumNumber).formatted()
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        let settingsVC = segue.source as? SettingsViewController
+        minimumValueLabel.text = settingsVC?.minimumValueTF.text
+        maximumValueLabel.text = settingsVC?.maximumValueTF.text
     }
 }
 
